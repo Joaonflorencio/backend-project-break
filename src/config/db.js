0140ad985e10fdc6e94119b514009ebb6c3fe3a8
João Neto <jflorencioadv@gmail.com>
2024-03-01 22:41:05 +0100

    project

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,7 @@
+require('dotenv').config(); // Esta linha deve ser uma das primeiras no arquivo
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://joaouser:kSbm5WHNr7R0NKKy@cluster0.rc2ekun.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const MONGO_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
@@ -12,7 +13,6 @@ const connectDB = async () => {
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-    // Salir de la aplicación con fallo
     process.exit(1);
   }
 };
